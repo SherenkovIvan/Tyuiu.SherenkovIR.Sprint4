20d9e05494c033035693cf9531831ace6a5afa80
Sherenkov Ivan <sherenkov12@gmail.com>
2025-11-04 18:43:53 +0500

    Task5

--- Tyuiu.SherenkovIR.Sprint4.Task5.V8/Program.cs
+++ Tyuiu.SherenkovIR.Sprint4.Task5.V8/Program.cs
@@ -1,23 +1,23 @@
-﻿using Tyuiu.SherenkovIR.Sprint4.Task4.V12.Lib;
+﻿using Tyuiu.SherenkovIR.Sprint4.Task5.V8.Lib;
 DataService ds = new DataService();
 Console.Title = "Спринт #4| Выполнил: Шеренков И. Р. | ИБКСБ-25-1";
 Console.WriteLine("****************************************************");
 Console.WriteLine("* Спринт #4                                        *");
-Console.WriteLine("*Тема:Двумерные массивы(Ввод с клавиатуры)         *");
-Console.WriteLine("* Задание #4                                       *");
-Console.WriteLine("* Вариант #12                                      *");
+Console.WriteLine("*Тема:Двумерные массивы(Генератор случайных чисел) *");
+Console.WriteLine("* Задание #5                                       *");
+Console.WriteLine("* Вариант #8                                       *");
 Console.WriteLine("* Выполнил: Шеренков Иван Романович | ИБКСБ-25-1   *");
 Console.WriteLine("****************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                         *");
 Console.WriteLine("*Дан двумерный целочисленный массив 5 на5 элементов*");
-Console.WriteLine("*заполненный значениями с клавиатуры в диапазоне от*");
-Console.WriteLine("*от 2 до 8 заменить четные элементы массива на 1.  *");
+Console.WriteLine("*заполненный случайными значениями   в диапазоне от*");
+Console.WriteLine("*от -3 до 4. Заменить отрицательные элементы на 0. *");
 Console.WriteLine("*                                                  *");
 Console.WriteLine("*                                                  *");
 Console.WriteLine("****************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                 *");
 Console.WriteLine("****************************************************");
-
+Random rnd = new Random();
 Console.WriteLine("Введите количество строк в массиве: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 
@@ -26,13 +26,11 @@ int columns = Convert.ToInt32(Console.ReadLine());
 
 int[,] mtrx = new int[rows, columns];
 Console.WriteLine("****************************************************");
-
 for (int i = 0; i < rows; i++)
 {
     for (int j = 0; j < columns; j++)
     {
-        Console.Write($"Введите{i},{j} элемент массива:");
-        mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+        mtrx[i, j] = rnd.Next(-3, 4);
     }
 }
 
@@ -51,15 +49,5 @@ Console.WriteLine("* РЕЗУЛЬТАТ:                                       *
 Console.WriteLine("****************************************************");
 int[,] res = ds.Calculate(mtrx);
 
-Console.WriteLine("Преобразованный массив:");
-for (int i = 0; i < rows; i++)
-{
-    for (int j = 0; j < columns; j++)
-    {
-        Console.Write($"{res[i, j]} \t");
-    }
-    Console.WriteLine();
-}
-
-Console.WriteLine();
+Console.WriteLine("Количество нулевых элементов = " + res);
 Console.ReadKey();
