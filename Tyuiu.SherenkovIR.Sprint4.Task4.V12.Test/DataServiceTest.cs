113e220819804256af076a41253f44c77588ffc4
Sherenkov Ivan <sherenkov12@gmail.com>
2025-11-04 15:56:20 +0500

    Task4

---
+++ Tyuiu.SherenkovIR.Sprint4.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.SherenkovIR.Sprint4.Task4.V12.Lib;
+namespace Tyuiu.SherenkovIR.Sprint4.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValdCalc()
+        {
+            DataService ds = new DataService();
+            int[,] mas4 = new int[5, 5] {{ 3, 6, 6, 8, 8 },
+            {2, 7, 2, 4, 6},
+            {3, 6, 2, 6, 4},
+            {8, 6, 2, 7, 3},
+            { 8, 7, 6, 6, 6}};
+            int[,] res = ds.Calculate(mas4);
+            int[,] wait = new int[5, 5] {
+            { 3, 1, 1, 1, 1 },
+            { 1, 7, 1, 1, 1 },
+            { 3, 1, 1, 1, 1 },
+            { 1, 1, 1, 7, 3 },
+            { 1, 7, 1, 1, 1 } };
+            CollectionAssert.AreEqual(wait, res);
+        }
+            
+    }
+}
---
+++ Tyuiu.SherenkovIR.Sprint4.Task4.V12/Program.cs
@@ -0,0 +1,65 @@
+﻿using Tyuiu.SherenkovIR.Sprint4.Task4.V12.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #4| Выполнил: Шеренков И. Р. | ИБКСБ-25-1";
+Console.WriteLine("****************************************************");
+Console.WriteLine("* Спринт #4                                        *");
+Console.WriteLine("*Тема:Двумерные массивы(Ввод с клавиатуры)         *");
+Console.WriteLine("* Задание #4                                       *");
+Console.WriteLine("* Вариант #12                                      *");
+Console.WriteLine("* Выполнил: Шеренков Иван Романович | ИБКСБ-25-1   *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                         *");
+Console.WriteLine("*Дан двумерный целочисленный массив 5 на5 элементов*");
+Console.WriteLine("*заполненный значениями с клавиатуры в диапазоне от*");
+Console.WriteLine("*от 2 до 8 заменить четные элементы массива на 1.  *");
+Console.WriteLine("*                                                  *");
+Console.WriteLine("*                                                  *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                 *");
+Console.WriteLine("****************************************************");
+
+Console.WriteLine("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество Столбцов в массиве: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] mtrx = new int[rows, columns];
+Console.WriteLine("****************************************************");
+
+for (int i = 0; i < rows; i++)
+{
+    for(int j = 0; j < columns; j++)
+    {
+        Console.Write($"Введите{i},{j} элемент массива:");
+        mtrx[i,j] = Convert.ToInt32(Console.ReadLine() );
+    }
+}
+
+Console.WriteLine("\nМассив:");
+for (int i = 0; i < rows; i++)
+{
+    for(int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mtrx[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("****************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                       *");
+Console.WriteLine("****************************************************");
+int[,] res = ds.Calculate(mtrx);
+
+Console.WriteLine("Преобразованный массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{res[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.ReadKey();
