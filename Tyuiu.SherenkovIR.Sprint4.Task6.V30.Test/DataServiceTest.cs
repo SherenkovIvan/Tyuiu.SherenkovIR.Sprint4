73582d6ffffb680aaa63d8f563c0d0e71ffa0cba
Sherenkov Ivan <sherenkov12@gmail.com>
2025-11-08 13:43:27 +0500

    Task6

---
+++ Tyuiu.SherenkovIR.Sprint4.Task6.V30.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SherenkovIR.Sprint4.Task6.V30.Lib;
+namespace Tyuiu.SherenkovIR.Sprint4.Task6.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            string[] array = new string[] { "Береза", "Дуб", "Клён", "Сосна", "Пихта", "Осина" };
+            var res = ds.Calculate(array);
+            string[] wait = { "Береза" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SherenkovIR.Sprint4.Task6.V30/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.SherenkovIR.Sprint4.Task6.V30.Lib;
+DataService ds = new DataService();
+
+string[] array = new string[] { "Береза", "Дуб", "Клён", "Сосна", "Пихта", "Осина" };
+
+Console.Title = "Спринт #4 | Выполнил: Шеренков И.Р. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Класс Array                                                       *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #30                                                             *");
+Console.WriteLine("* Выполнил: Шеренков Иван Романович  | ИБКСб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан строковый массив данных, используя класс Array,                     *");
+Console.WriteLine("* выведите элементы массива, длина которых больше 5 символов.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}\t ");
+}
+
+string[] res = ds.Calculate(array);
+
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.Write("Элементы, длина которых > 5: ");
+
+for (int i = 0; i < res.Length; i++)
+{
+    Console.Write(res[i]);
+}
+
+Console.WriteLine();
+
+Console.ReadKey();
